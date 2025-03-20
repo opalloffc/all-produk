45ace8e9864c2cb40313aebfbe66b1e5b27b5b65
opalloffc <naufalakbr12@gmail.com>
2025-03-20 08:42:37 +0700

    Update produk-app.js

--- js/produk-app.js
+++ js/produk-app.js
@@ -9,10 +9,11 @@ document.addEventListener("alpine:init", () => {
         img: "vps.jpg",
         price: 30000,
         discount: 10,
+        stock: 0
       },
-      { id: 2, name: "VPS Ram 4 Core 2", img: "vps.jpg", price: 40000 },
-      { id: 3, name: "VPS Ram 8 Core 4", img: "vps.jpg", price: 50000 },
-      { id: 4, name: "VPS Ram 16 Core 4", img: "vps.jpg", price: 60000 },
+      { id: 2, name: "VPS Ram 4 Core 2", img: "vps.jpg", price: 40000, stock: 0 },
+      { id: 3, name: "VPS Ram 8 Core 4", img: "vps.jpg", price: 50000, stock: 0 },
+      { id: 4, name: "VPS Ram 16 Core 4", img: "vps.jpg", price: 60000, stock: 0 },
       { id: 5, name: "Ram 1GB", img: "panel.jpg", price: 1000 },
       { id: 6, name: "Ram 2GB", img: "panel.jpg", price: 2000 },
       { id: 7, name: "Ram 3GB", img: "panel.jpg", price: 3000 },
@@ -36,12 +37,12 @@ document.addEventListener("alpine:init", () => {
       { id: 20, name: "OpallBotz-V2", img: "wabot.jpg", price: 45000 },
       { id: 21, name: "Owner Script", img: "wabot.jpg", price: 50000 },
       { id: 22, name: "Nokos Indo", img: "nokos.jpg", price: 5000 },
-      { id: 23, name: "function database", img: "security.jpg", price: 45000 },
+      { id: 23, name: "function database", img: "security.jpg", price: 50000 },
     ],
 
     //list services
     services: [
-      {id: 1, name: "Jasa Add Database", img: "security.jpg", price: 45000, discount: 10 },
+      {id: 1, name: "Jasa Add Database", img: "security.jpg", price: 50000, discount: 50 },
       {id: 2, name: "Jasa Install Panel Pterodactyl", img: "panel.jpg", price: 5000},
       {id: 2, name: "Jasa Tema Panel Pterodactyl", img: "panel.jpg", price: 5000},
     ],
